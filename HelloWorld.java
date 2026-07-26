6b5241b4f5108f94f9bac8373e88bd6c0d095049
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-26 13:44:59 +0530

    Create HelloWorld class with main method
    Fix capitalization issues in HelloWorld class.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(string[] args) {
+    system.out.println("Hello,World!");
+    system.out.println("My name is Pranjal Dalavi.");
+    system.out.println("I am learning Java.")
+      }
+}
