ab7da5edf6d10da3d8ebeb77ab817a8b7848969f
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-27 17:59:54 +0530

    Correct variable types and print statements
    Fix variable declarations and output statements in Variables.java.

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+  public static void main(string[] args) {
+
+  string name = "Pranjal";
+    int age = 18;
+    double hieght = 5.8;
+    char grade = 'A';
+    boolen isStudent = true;
+
+  system.out.print("Name:" +name);
+  system.out.print("Age:" +age);
+  system.out.print("Height:" +height);
+  system.out.print("Grade:" +grade);
+  system.out.print("Student:" +isStudent);
+  }
+}
