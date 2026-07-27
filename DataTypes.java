43dc1a57e76a02ea7fbfc7c75a6eb80d30f36dc6
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-27 18:07:21 +0530

    Correct variable declarations and print statements
    Fix variable types and method calls in DataTypes.java.

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+  public static void main(string[] args) {
+
+  string name = "Pranjal";
+    int age = 18;
+    double hieght = 5.8;
+    char grade = 'A';
+    boolen isStudent = true;
+
+  system.out.println("Name:" +name);
+  system.out.println("Age:" +age);
+  system.out.println("Height:" +height);
+  system.out.println("Grade:" +grade);
+  system.out.println("Student:" +isStudent);
+  }
+}
