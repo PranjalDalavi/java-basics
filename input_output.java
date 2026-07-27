0eb6477bae2a1fe2980ff3eb28d45608ac4370fa
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-27 18:16:23 +0530

    Implement user input for name and age
    Fixes multiple issues in the InputOutput class for user input.

---
+++ input_output.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InputOutput{
+  public static void main(string[] args){
+    scanner sc = new Scanner(system.in);
+    System.out.print("Enter your nam:");
+    String name = sc.nextline();
+    system.out.print("Enter your age:");
+    int age = sc.nextline();
+    System.out.println("Hello," +name);
+    System.out.println("You are"+ age +"years old");
+    sc.close();
+  }
+}
+    
