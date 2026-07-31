e3c988e139a474b3bbac8cfec4aaba75ef6ce0ea
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-31 20:57:36 +0530

    Create NestedIf.java

---
+++ NestedIf.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NestedIf {
+  public static void main(String[] args) {
+
+  Scanner sc = new Scanner(System in);
+
+  System.out.print("Enter your age:");
+  int age = sc.nextInt();
+
+  System.out.print("Do you have a driving license?(true/false):");
+    boolen license = sc.nextBoolen();
+
+    if(age>=18) {
+      if (license0 {
+          System.out.println("You can drive.");
+      }
+      } else {
+        System.out.println("You are old enough,but you need a driving license.");
+      }
+      sc.close();
+    }
+  }
+  
