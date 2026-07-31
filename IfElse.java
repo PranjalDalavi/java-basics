e3f9674b8b445ada2a9d9fe191211f61ea8c5427
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-31 17:39:05 +0530

    Implement voting eligibility check based on age
    Fix input handling and output messages for voting eligibility.

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IfElse {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(system.in);
+    int age = sc.nextline();
+
+  if(age>=18) {
+    System.out.println("You are eligible to vote:");
+  } else {
+    System.out.println("You are not eligible to vote:");
+  }
+    sc.close();
+  }
+}
+    
