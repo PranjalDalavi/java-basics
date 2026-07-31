c10019a796776c8fe0140b40f853eefe9d76a5aa
PranjalDalavi <pranjal.dalavi04@gmail.com>
2026-07-31 21:09:57 +0530

    Add Arrays class with main method to print elements

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+        System.out.println("Third element: " + numbers[2]);
+        System.out.println("Fourth element: " + numbers[3]);
+        System.out.println("Fifth element: " + numbers[4]);
+    }
+}
